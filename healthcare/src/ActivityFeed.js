542766a1ba835381750bfa97db4b039a2de55bcd
murariraj <murariraj2002@gmail.com>
2025-05-28 01:50:31 +0530

    Fitpeo Assignment

---
+++ healthcare/src/ActivityFeed.js
@@ -0,0 +1,137 @@
+import React from 'react'
+
+function ActivityFeed() {
+    return (
+        <div style={{ border: "1px solid black", display: "flex", position: 'absolute', bottom: "30px", width: "440px", height: "170px", left: "560px", }}>
+            <span style={{ color: "gray" }}>Activity</span>
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "20px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "35px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "30px" }}>Mon</span>
+
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "50px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "50px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "65px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "62px", width: "5px", borderRadius: "20px", position: "absolute", left: "78px", top: "20px", backgroundColor: "purple", borderColor: "purple" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "90px" }}>Tue</span>
+            <div style={{ border: "1px solid black", height: "62px", width: "5px", borderRadius: "20px", position: "absolute", left: "78px", top: "95px", backgroundColor: "purple", borderColor: "purple" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "90px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "105px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "105px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "140px" }}>Wed</span>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "120px", top: "105px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "135px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "150px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "165px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "190px" }}>Thu</span>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "165px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "180px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "195px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "210px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "240px" }}>Tue</span>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "225px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "225px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "240px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "255px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "290px" }}>Fri</span>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "270px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "285px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "285px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "300px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "340px" }}>Sat</span>
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "315px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "330px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "345px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "345px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <span style={{ color: "gray", position: "absolute", top: "150px", left: "390px" }}>Sun</span>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "360px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "125px", width: "5px", borderRadius: "20px", position: "absolute", left: "375px", top: "20px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "70px", width: "5px", borderRadius: "20px", position: "absolute", left: "390px", top: "44px", backgroundColor: "blueviolet", borderColor: "blueviolet" }}>
+
+            </div>
+
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "405px", top: "54px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "35px", width: "5px", borderRadius: "20px", position: "absolute", left: "405px", top: "100px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+            <div style={{ border: "1px solid black", height: "40px", width: "5px", borderRadius: "20px", position: "absolute", left: "420px", top: "105px", backgroundColor: "gray", borderColor: "gray" }}>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ActivityFeed;
